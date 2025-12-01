cc97bf26c5ae3829c42be4104600f1494529011a
Supreet <supreetkumar32@gmail.com>
2025-12-01 22:56:29 +0530

    auditing basics

--- src/main/java/com/weekfour/production_ready_features/prod_ready_features/config/AppConfig.java
+++ src/main/java/com/weekfour/production_ready_features/prod_ready_features/config/AppConfig.java
@@ -3,8 +3,10 @@ package com.weekfour.production_ready_features.prod_ready_features.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
+@EnableJpaAuditing
 public class AppConfig {
 
     @Bean
--- src/main/java/com/weekfour/production_ready_features/prod_ready_features/entities/PostEntity.java
+++ src/main/java/com/weekfour/production_ready_features/prod_ready_features/entities/PostEntity.java
@@ -2,6 +2,13 @@ package com.weekfour.production_ready_features.prod_ready_features.entities;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "posts")
@@ -9,6 +16,7 @@ import lombok.*;
 @NoArgsConstructor
 @Getter
 @Setter
+@EntityListeners(AuditingEntityListener.class)
 public class PostEntity {
 
     @Id
@@ -16,4 +24,17 @@ public class PostEntity {
     private Long id;
     private String title;
     private String description;
+
+    @CreatedDate
+    @Column(nullable=false, updatable=false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime updatedDate;
+
+    @CreatedBy
+    private String createdBy;
+
+    @LastModifiedBy
+    private String updatedBy;
 }
