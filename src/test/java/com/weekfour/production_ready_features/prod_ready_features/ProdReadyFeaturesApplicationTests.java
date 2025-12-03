d666e7c151387327b91fbc5c9d1915af5055b910
Supreet <supreetkumar32@gmail.com>
2025-12-03 21:51:11 +0530

    using get in rest client

--- src/test/java/com/weekfour/production_ready_features/prod_ready_features/ProdReadyFeaturesApplicationTests.java
+++ src/test/java/com/weekfour/production_ready_features/prod_ready_features/ProdReadyFeaturesApplicationTests.java
@@ -1,13 +1,26 @@
 package com.weekfour.production_ready_features.prod_ready_features;
 
+import com.weekfour.production_ready_features.prod_ready_features.clients.EmployeeClient;
+import com.weekfour.production_ready_features.prod_ready_features.dto.EmployeeDTO;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class ProdReadyFeaturesApplicationTests {
 
+	@Autowired
+	private EmployeeClient employeeClient;
+
+	//we hav eto built controller...but for learning purpose we are using tests
 	@Test
-	void contextLoads() {
+	void getAllEmployeesTest() {
+		List<EmployeeDTO> employeeDTOList = employeeClient.getAllEmployees();
+		System.out.println(employeeDTOList);
 	}
+
 
 }
